7d7301f693f34b7c51ba6d3411589ed3825813a1
Daniel Mckee <dmg6546@rit.edu>
2023-10-05 18:14:18 -0400

    yeah

--- ADSaA/Assignments/A02/IRSystem.java
+++ ADSaA/Assignments/A02/IRSystem.java
@@ -1,9 +1,9 @@
 package Assignments.A02;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 class TermEntry{
     String term;
@@ -18,21 +18,40 @@ class TermEntry{
 }
 
 public class IRSystem {
+    HashMap<String, Integer> countMap = new HashMap<>();
     
     public String[] parse(String filename) throws IOException{
         FileReader reader = new FileReader(filename);
         BufferedReader bufferedReader = new BufferedReader(reader);
-        String quoteStrings ="";
-        while ((quoteStrings+=bufferedReader.readLine()) != null){
-            continue;
+        String readerString ="";
+        String holdString = "";
+        while((readerString=bufferedReader.readLine())!=null){
+            holdString+= readerString + " ";
         }
-        System.out.println(quoteStrings.toString());
+        holdString = holdString.toLowerCase();
+        String[] returnTokens = holdString.split(" ");
         bufferedReader.close();
-        return null;
+        return returnTokens;
+    }
+
+    public void countTerms(String[] terms){
+
+        for (String element : terms){
+            if (countMap.containsKey(element)){
+                countMap.replace(element, countMap.get(element).intValue()+1);
+            }
+            if (!(countMap.containsKey(element))){
+                countMap.put(element, 1);
+            }
+        }
+        System.out.println(countMap.toString());
+
     }
 
     public static void main(String[] args) throws IOException {
         IRSystem irSystem = new IRSystem();
-        irSystem.parse("ADSaA\\Assignments\\A02\\quotes.txt");
+        String[] terms = irSystem.parse("ADSaA\\Assignments\\A02\\quotes.txt");
+
+        irSystem.countTerms(terms);
     }
 }
