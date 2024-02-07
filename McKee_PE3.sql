f3d0606732d43f83aa1f957ca8092f2e6ac95bc9
Kaiirros <dgm6546@rit.edu>
2024-02-07 18:30:15 -0500

    changes

--- McKee_PE3.sql
+++ McKee_PE3.sql
@@ -1,36 +1,36 @@
 
 -- Task 1
-SELECT title, length
+SELECT title AS Title, length AS Length
 FROM film
 WHERE description LIKE '%fun%' AND length <= 120;
 
 -- Task 2
-SELECT title
+SELECT title AS Title
 FROM film
 WHERE title LIKE '%a' OR title LIKE '%e' OR title LIKE '%i' OR title LIKE '%o' OR title LIKE '%u';
 
 -- Task 3
-SELECT title
+SELECT title AS Title
 FROM film
 WHERE length >= 120 AND length <= 180 ;
 
 -- Task 4
-SELECT title, length
+SELECT title AS Title, length AS Length
 FROM film
 WHERE title LIKE '%river%' AND (rating = 'PG' OR rating = 'PG-13');
 
 -- Task 5
-SELECT title
+SELECT title AS Title
 FROM film
-WHERE releaseYear > 2012 AND length > 160;
+WHERE releASeYear > 2012 AND length > 160;
 
 --Task 6
-SELECT title, replacementCost, rating
+SELECT title AS Title, replacementCost AS Replacement Cost, rating AS Rating
 FROM film
 WHERE replacementCost = 19.99 AND title NOT LIKE '_a%';
 
 -- Task 7
-SELECT title
+SELECT title AS Title
 FROM film
 WHERE description IS NULL;
 
@@ -40,7 +40,7 @@ FROM film
 WHERE title = 'Town Ark';
 
 --Task 9
-SELECT title
+SELECT title AS Title
 FROM film
 WHERE (rating = 'G' AND releaseYear = 2006) OR (rating = 'PG' AND releaseYear = 2010);
 
